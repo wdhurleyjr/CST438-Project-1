615931726b4ab9d5dc4d2d3ecfa42346710ffdf6
SebasX5 <sebastian.santoyopro@gmail.com>
2024-09-10 11:13:11 -0700

    Updated App.js
    Returned App.js to Main's code

--- App.js
+++ App.js
@@ -1,36 +1,11 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import AppNavigator from './src/navigation/AppNavigator';
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <AppNavigator />
-//     </NavigationContainer>
-//   );
-// }
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import AppNavigator from './src/navigation/AppNavigator';
 
-import MultiTeamScreen from './src/screens/MultiTeamScreen';
-import LoginScreen from './src/screens/LoginScreen';
-
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="MultiTeam" component={MultiTeamScreen} />
-        
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
-};
-
-export default App;
-
-
+}
